5a5eb5a0edc526f928ca6e42994795138e97f173
Islam Yusuf <yusufislam282@gmail.com>
2021-08-23 21:09:51 +0300

    Implemented dashboard

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
-import { CART_LOAD_ITEMS, CART_SAVE_ITEMS, CART_REMOVE_SHIPPING_ADDRESS } from '../constants/cartConstants';
+import { CART_LOAD_ITEMS, CART_SAVE_ITEMS, CART_REMOVE_SHIPPING_ADDRESS, CART_ADD_ITEM } from '../constants/cartConstants';
 import {
     USER_DETAILS_FAIL,USER_DETAILS_REQUEST,USER_DETAILS_SUCCESS,
     USER_SIGNIN_FAIL,USER_SIGNIN_REQUEST,USER_SIGNIN_SUCCESS,
@@ -10,21 +10,17 @@ import {
     USER_UPDATE_SUCCESS,USER_UPDATE_FAIL
 } from '../constants/userConstants';
 
-export const signin = (email, password) => async (dispatch) =>{
+export const signin = (email, password) => async (dispatch, getState) =>{
     dispatch({type: USER_SIGNIN_REQUEST, payload: {email, password}});
-
     try {
         const {data} = await Axios.post('/api/users/signin', {email, password});
         const savedCartItems = data.cartItems;
-        const existingCartItems = localStorage.getItem('cartItems')
-            ? JSON.parse(localStorage.getItem('cartItems'))
-            : []
-        const cartItems = [...savedCartItems,...existingCartItems];
         dispatch({type: USER_SIGNIN_SUCCESS, payload: data});
-        dispatch({type: CART_LOAD_ITEMS, payload: cartItems});
         localStorage.setItem('userInfo', JSON.stringify(data));
-        localStorage.setItem('cartItems',JSON.stringify(cartItems)); 
-
+        
+        if(savedCartItems) for(let i=0; i < savedCartItems.length; ++i) dispatch({type:CART_ADD_ITEM, payload:savedCartItems[i]});
+        const {cart:{cartItems}} = getState();
+        localStorage.setItem('cartItems',JSON.stringify(cartItems));
     } catch (e) {
         dispatch({type: USER_SIGNIN_FAIL,
             payload: e.response && e.response.data.message
--- server/routers/orderRouter.js
+++ server/routers/orderRouter.js
@@ -7,6 +7,48 @@ import { isAuth, isAdmin } from '../utils.js';
 
 const orderRouter = express.Router();
 
+orderRouter.get('/summary',isAuth,isAdmin,
+    expressAsyncHandler(async (req, res) => {
+      const orders = await Order.aggregate([
+        {
+          $group: {
+            _id: null,
+            numOrders: { $sum: 1 },
+            totalSales: { $sum: '$totalPrice' },
+          },
+        },
+      ]);
+      const users = await User.aggregate([
+        {
+          $group: {
+            _id: null,
+            numUsers: { $sum: 1 },
+          },
+        },
+      ]);
+      const dailyOrders = await Order.aggregate([
+        {
+          $group: {
+            _id: { $dateToString: { format: '%Y-%m-%d', date: '$createdAt' } },
+            orders: { $sum: 1 },
+            sales: { $sum: '$totalPrice' },
+          },
+        },
+        { $sort: { _id: 1 } },
+      ]);
+      const productCategories = await Product.aggregate([
+        {
+          $group: {
+            _id: '$category',
+            count: { $sum: 1 },
+          },
+        },
+      ]);
+      res.send({ users, orders, dailyOrders, productCategories });
+    })
+  );
+
+
 orderRouter.get('/list', isAuth, expressAsyncHandler(async (req, res) => {
     const orders = await Order.find({user: req.user._id});
     res.send(orders);
@@ -101,47 +143,6 @@ orderRouter.put(
     })
 );
 
-//getting a list of all the orders
-orderRouter.get('/summary',isAuth,isAdmin,
-    expressAsyncHandler(async (req, res) => {
-      const orders = await Order.aggregate([
-        {
-          $group: {
-            _id: null,
-            numOrders: { $sum: 1 },
-            totalSales: { $sum: '$totalPrice' },
-          },
-        },
-      ]);
-      const users = await User.aggregate([
-        {
-          $group: {
-            _id: null,
-            numUsers: { $sum: 1 },
-          },
-        },
-      ]);
-      const dailyOrders = await Order.aggregate([
-        {
-          $group: {
-            _id: { $dateToString: { format: '%Y-%m-%d', date: '$createdAt' } },
-            orders: { $sum: 1 },
-            sales: { $sum: '$totalPrice' },
-          },
-        },
-        { $sort: { _id: 1 } },
-      ]);
-      const productCategories = await Product.aggregate([
-        {
-          $group: {
-            _id: '$category',
-            count: { $sum: 1 },
-          },
-        },
-      ]);
-      res.send({ users, orders, dailyOrders, productCategories });
-    })
-  );
 
 
 export default orderRouter;
